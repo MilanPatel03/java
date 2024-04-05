a177f6390e09caba2c2436a4fe9baa0cfd54af27
Milan Patel <83775666+MilanPatel03@users.noreply.github.com>
2024-04-05 21:53:21 +0530

    Add files via upload

---
+++ javaprojects/folder01/java lrn/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name = "Parth";
+		hello(name);
+		hello(name);
+		hello(name);
+
+	}
+
+	static void hello(String title) {
+		System.out.println("Hello there! " + title );
+	}
+}
+
+
